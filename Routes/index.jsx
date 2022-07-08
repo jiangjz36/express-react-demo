89946e142f89953811d626851b920ce1b9c451dd
jiangjz <jiangjz36@163.com>
2022-07-08 15:28:33 +0800

    fix: 添加404页面

--- Routes/index.jsx
+++ Routes/index.jsx
@@ -5,9 +5,15 @@ import { Routes, Route } from 'react-router-dom'
 import Home from '@/containers/Home'
 import Login from '@/containers/Login'
 
+const Page404 = () => (
+  <div>page 404</div>
+)
+
 export default (
   <Routes>
     <Route path='/' exact element={<Home />}></Route>
+    <Route path='/home' exact element={<Home />}></Route>
     <Route path='/login' exact element={<Login />}></Route>
+    <Route path='*' exact element={<Page404 />}></Route>
   </Routes>
 )
